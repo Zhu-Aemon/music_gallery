7ae81ff4620d46c55a2aa2c8b723b78fe293db86
TianZhu_Wang <stephen_einstein@iCloud.com>
2022-10-17 17:41:11 +0800

    initialize

---
+++ GUI.py
@@ -0,0 +1,69 @@
+from PySide2.QtWidgets import QApplication, QMessageBox, QFileDialog, QWidget, QPushButton, QListWidget
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtGui import QPalette, QColor, QPainter, QBrush, QImage, QPixmap, QIcon
+from PySide2.QtCore import Qt, QRect, QSize
+
+
+class MainWindow:
+
+    def __init__(self):
+        self.ui = QUiLoader().load("MainWindow.ui")
+        self.ui.setWindowTitle("Music Gallery")
+        palette = self.ui.palette()
+        palette.setColor(QPalette.Background, '#ffffff')
+        self.ui.setPalette(palette)
+        self.ui.setAutoFillBackground(True)
+        self.set_icon(self.ui.settings, r'resources/setting_button.png')
+        self.set_icon(self.ui.add, r'resources/add.png')
+        self.set_list_icon(exception=None)
+        self.ui.listWidget.itemClicked.connect(self.change_icon)
+
+    def set_icon(self, button, icon):
+        img = QImage(icon)
+        pixmap = QPixmap(img)
+        fit_pixmap = pixmap.scaled(23, 23, Qt.IgnoreAspectRatio,
+                                  Qt.SmoothTransformation)  # 注意 scaled() 返回一个 QtGui.QPixmap
+        button.setIcon(fit_pixmap)
+        button.setIconSize(QSize(23, 23))
+
+    def set_list_icon(self, exception: int):
+        icon_list = ['resources/all_music.png', 'resources/favourite_music.png', 'resources/recommended.png',
+                     'resources/MV.png', 'resources/bilibili.png']
+        if exception is not None:
+            for i in range(self.ui.listWidget.count()):
+                if i != exception:
+                    img = QImage(icon_list[i])
+                    pixmap = QPixmap(img)
+                    fit_pixmap = pixmap.scaled(23, 23, Qt.IgnoreAspectRatio,
+                                               Qt.SmoothTransformation)  # 注意 scaled() 返回一个 QtGui.QPixmap
+                    self.ui.listWidget.item(i).setIcon(fit_pixmap)
+                else:
+                    continue
+            self.ui.listWidget.setIconSize(QSize(23, 23))
+        else:
+            for i in range(self.ui.listWidget.count()):
+                img = QImage(icon_list[i])
+                pixmap = QPixmap(img)
+                fit_pixmap = pixmap.scaled(23, 23, Qt.IgnoreAspectRatio,
+                                            Qt.SmoothTransformation)  # 注意 scaled() 返回一个 QtGui.QPixmap
+                self.ui.listWidget.item(i).setIcon(fit_pixmap)
+            self.ui.listWidget.setIconSize(QSize(23, 23))
+
+    def change_icon(self):
+        icon_list = ['resources-inverted/all_music.png', 'resources-inverted/favourite_music.png',
+                     'resources-inverted/recommended.png',
+                     'resources-inverted/MV.png', 'resources-inverted/bilibili.png']
+        index = self.ui.listWidget.currentRow()
+        img = QImage(icon_list[index])
+        pixmap = QPixmap(img)
+        fit_pixmap = pixmap.scaled(23, 23, Qt.IgnoreAspectRatio,
+                                   Qt.SmoothTransformation)
+        self.ui.listWidget.item(index).setIcon(fit_pixmap)
+        self.ui.listWidget.setIconSize(QSize(23, 23))
+        self.set_list_icon(exception=index)
+
+
+app = QApplication([])
+mw = MainWindow()
+mw.ui.show()
+app.exec_()
